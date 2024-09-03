928f99d308aa8dd635a02b9c20b9cdf16aa441ec
Admin-github123 <patelsiddhant9297@gmail.com>
2024-09-03 18:38:54 +0530

    main

--- src/main/java/com/otp_service/controller/SampleService.java
+++ src/main/java/com/otp_service/controller/SampleService.java
@@ -2,6 +2,7 @@ package com.otp_service.controller;
 
 public class SampleService {
     public String getMessage() {
-        return "Message from developer branch";
+
+        return "Message from main branch";
     }
 }
